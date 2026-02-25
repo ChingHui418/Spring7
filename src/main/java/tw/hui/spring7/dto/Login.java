889b4ca2be66e575a609f82fcb25a42ea81c1f2f
ChingHui418 <a0985218998@gmail.com>
2026-02-25 12:02:52 +0800

    20260225-1202/chat

---
+++ src/main/java/tw/hui/spring7/dto/Login.java
@@ -0,0 +1,8 @@
+package tw.hui.spring7.dto;
+
+import lombok.Data;
+
+@Data
+public class Login {
+	private String email, passwd;
+}
---
+++ src/main/java/tw/hui/spring7/response/ChatMessage.java
@@ -0,0 +1,8 @@
+package tw.hui.spring7.response;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+	private String email, content, time;
+}
